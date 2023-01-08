e08794115df42791b476932d13167e677a3c6010
Sérgio Dantas <dantas.sergio@escolar.ifrn.edu.br>
2023-01-08 20:44:57 -0300

    factory function alterada para constructor function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,84 @@
-function criaCalculadora() {
-  const calculadora = {
-    display: document.querySelector('.display'),
+function Calculadora() {
+  this.display = document.querySelector('.display');
+  
+  this.limpaDisplay = function () {
+    this.display.value = '0';
+  };
 
-    adicionaAoDisplay(valor) {
-      if (this.display.value === '0') {
-        if (valor === '0') return
-        const operacoes = ['**', '/', '*', '-', '+', '%', '.']
-        if (operacoes.indexOf(valor) === -1) this.display.value = ''
-      } else if (this.display.value === 'Conta Inválida') {
-        this.display.value = ''
-      }
-      this.display.value += valor
-    },
+  this.mostraContaInvalida = function () {
+    this.display.value = 'Conta Inválida';
+  };
 
-    realizaCalculo() {
-      try {
-        const calculo = eval(this.display.value)
-        this.display.value = calculo
-      } catch {
-        this.mostraContaInvalida()
-      }
-    },
+  this.apagaUltimoValor = function () {
+    this.display.value = this.display.value.slice(0, -1);
+  };
 
-    setaFuncionalidadesBotoes() {
-      document.addEventListener('click', e => {
-        const elemento = e.target
-        if (elemento.classList.contains('num')) {
-          this.adicionaAoDisplay(elemento.innerText)
-        } else {
-          const id = elemento.id
-          switch (id) {
-            case 'c':
-              this.limpaDisplay()
-              break
-            case 'backspace':
-              this.apagaUltimoValor()
-              break
-            case 'potenciacao':
-              this.adicionaAoDisplay('**')
-              break
-            case 'divisao':
-              this.adicionaAoDisplay('/')
-              break
-            case 'multiplicacao':
-              this.adicionaAoDisplay('*')
-              break
-            case 'subtracao':
-              this.adicionaAoDisplay('-')
-              break
-            case 'soma':
-              this.adicionaAoDisplay('+')
-              break
-            case 'resto':
-              this.adicionaAoDisplay('%')
-              break
-            case 'ponto':
-              this.adicionaAoDisplay('.')
-              break
-            case 'igual':
-              this.realizaCalculo()
-              break
-          }
+  this.pegaValorDisplay = function () {
+    return this.display.value;
+  };
+
+  this.realizaCalculo = function () {
+    try {
+      const calculo = eval(this.pegaValorDisplay());
+      this.display.value = calculo;
+    } catch {
+      this.mostraContaInvalida();
+    }
+  };
+
+  this.adicionaAoDisplay = function (valor) {
+    if (this.display.value === '0') {
+      if (valor === '0') return;
+      if (['**', '/', '*', '-', '+', '%', '.'].indexOf(valor) === -1) this.display.value = '';
+    }
+    if (this.display.value === 'Conta Inválida') this.display.value = '';
+    this.display.value += valor;
+  };
+
+  this.adicionaFuncionalidadesBotoes = function () {
+    document.addEventListener('click', (e) => {
+      const elemento = e.target;
+      if (elemento.classList.contains('num')) {
+        this.adicionaAoDisplay(elemento.innerText);
+      } else {
+        const id = elemento.id;
+        switch (id) {
+          case 'c':
+            this.limpaDisplay();
+            break;
+          case 'backspace':
+            this.apagaUltimoValor();
+            break;
+          case 'potenciacao':
+            this.adicionaAoDisplay('**');
+            break;
+          case 'divisao':
+            this.adicionaAoDisplay('/');
+            break;
+          case 'multiplicacao':
+            this.adicionaAoDisplay('*');
+            break;
+          case 'subtracao':
+            this.adicionaAoDisplay('-');
+            break;
+          case 'soma':
+            this.adicionaAoDisplay('+');
+            break;
+          case 'resto':
+            this.adicionaAoDisplay('%');
+            break;
+          case 'ponto':
+            this.adicionaAoDisplay('.');
+            break;
+          case 'igual':
+            this.realizaCalculo();
+            break;
         }
-      })
-    },
+      }
+    });
+  };
 
-    limpaDisplay() {
-      this.display.value = '0'
-    },
-
-    mostraContaInvalida() {
-      this.display.value = 'Conta Inválida'
-    },
-
-    apagaUltimoValor() {
-      if (this.display.value === '0') return
-      this.display.value = this.display.value.slice(0, -1)
-    },
-  }
-  return calculadora
+  this.adicionaFuncionalidadesBotoes();
 }
 
-const calculadora = criaCalculadora()
-calculadora.setaFuncionalidadesBotoes()
+new Calculadora();
